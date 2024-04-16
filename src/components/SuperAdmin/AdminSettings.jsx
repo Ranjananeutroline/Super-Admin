bdc30c66ac2f7f1fb2b7e0cdb5c8296431e00de6
Ranjana <Ranjana.neutroline@gmail.com>
2024-04-16 16:45:54 +0545

    upto-16th april setting's notification UI done

--- src/components/SuperAdmin/AdminSettings.jsx
+++ src/components/SuperAdmin/AdminSettings.jsx
@@ -1,6 +1,10 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import { RiCloseLine } from 'react-icons/ri';
+import "./superAdminDashboard.css";
 
-const AdminSettings = () => {
+const AdminSettings = ({ show, onHide }) => {
   const [activeTab, setActiveTab] = useState('Account');
   const [showModal, setShowModal] = useState(false);
   const [notifications, setNotifications] = useState([]);
@@ -55,152 +59,145 @@ const AdminSettings = () => {
     updatedNotifications.splice(index, 1);
     setNotifications(updatedNotifications);
   };
+
   return (
     <div className="w-full px-[50px] md:px-10 dashboard-main">
-             <h1 className="text-[27px] text-[#3F26A5] pb-2 ">Settings</h1>
+      <h1 className="text-[27px] text-[#3F26A5] pb-2 ">Settings</h1>
 
-            <div className="flex gap-3 mt-2 mb-3 ">
-                
-                <h2 
-                    className={`cursor-pointer font-sans text-[18px] ${activeTab === 'Account' ? 'underline' : ''} ${activeTab === 'Account' ? 'text-blue-600 ' : 'text-gray-600 hover:text-blue-800'}`}
-                    onClick={() => setActiveTab('Account')}
-                >
-                    Account Settings
-                </h2>
-                <h2 
-                    className={`cursor-pointer font-sans text-[18px] ${activeTab === 'Notification' ? 'underline' : ''} ${activeTab === 'Notification' ? 'text-blue-600 ' : 'text-gray-600 hover:text-blue-800'}`}
-                    onClick={() => setActiveTab('Notification')}
-                >
-                    Notification Settings
-                </h2>
-            
-            
-           
-            {activeTab === 'Account' && (
-                <div className="pb-4 md:pb-20 accountsettings">
-                    {/* Account Settings Content */}
-                </div>
-            )}
-        
-            {activeTab === 'Notification' && (
-                <div className="pb-4 md:pb-20 notificationsettings">
-                    {/* Notification Settings Content */}
-                </div>
-            )}
-        </div>
-         
-      
-    
+      <div className="flex gap-4 mt-2 mb-3 ">
+        <h2
+          className={`cursor-pointer font-sans text-[18px] ${activeTab === 'Account' ? 'underline' : ''} ${
+            activeTab === 'Account' ? 'text-blue-800 ' : 'text-gray-600 hover:text-blue-800'
+          }`}
+          onClick={() => setActiveTab('Account')}
+        >
+          Account Settings
+        </h2>
+        <h2
+          className={`cursor-pointer font-sans text-[18px] ${activeTab === 'Notification' ? 'underline' : ''} ${
+            activeTab === 'Notification' ? 'text-blue-800 ' : 'text-gray-600 hover:text-blue-800'
+          }`}
+          onClick={() => setActiveTab('Notification')}
+        >
+          Notification Settings
+        </h2>
+        <h2
+          className={`cursor-pointer font-sans text-[18px] ${activeTab === 'Profile' ? 'underline' : ''} ${
+            activeTab === 'Profile' ? 'text-blue-800 ' : 'text-gray-600 hover:text-blue-800'
+          }`}
+          onClick={() => setActiveTab('Profile')}
+        >
+          Profile Settings
+        </h2>
+      </div>
 
-{activeTab === 'Account' && (
-    <div className="pb-4 md:pb-20 accountsettings">
-      {/* Account Settings Content */}
-    </div>
-  )}
+      {activeTab === 'Account' && <div className="pb-4 md:pb-20 accountsettings">{/* Account Settings Content */}</div>}
 
-       {activeTab === 'Notification' && (
-          <div className="notificationsettings">
-            <div className="flex justify-end">
-              <button
-                className="mt-2 mr-4 bg-blue-500 hover:bg-blue-700 text-white px-4 py-2 rounded-md font-sans "
-                onClick={openModal}
-              >
-                Add Notification
-              </button>
-            </div>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 mt-4 border-[1px]">
-              {notifications.map((notification, index) => (
-                <div key={index} className="bg-[#ffffff] rounded-[8px] w-[320px] h-auto shadow-sm border-1px border-[#CF9ECF40] ">
-                  <div className="h-[50px] bg-[#CF9ECF40] rounded-[8px] p-2 flex items-center justify-center">
-                  <h2 className="text-lg ">{notification.title}</h2>
-                  </div>
-                <div className="my-2 mx-2 flex items-center justify-center">
-                <p className="text-sm">{notification.body}</p>
-                </div>
-               
-                  <div className="flex justify-end mt-2 mx-4 my-2 ">
-                    <button
-                      className="bg-blue-500 hover:bg-blue-700 text-white px-1 py-[2px] rounded-md mr-2 text-[12px] font-sans w-[50px] "
-                      onClick={() => handleEdit(index)}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      className="bg-[#ec7d7d] hover:bg-[#ee6161] text-white px-1 py-[2px] rounded-md text-[12px] font-sans w-[50px]"
-                      onClick={() => handleDelete(index)}
-                    >
-                      Delete
-                    </button>
-                  </div>
-                 
-                </div>
-              ))}
-            </div>
+      {activeTab === 'Notification' && (
+        <div className="pb-4 md:pb-20 notificationsettings">
+          {/* Notification Settings Content */}
+          <div className="flex justify-end">
+            <button
+              className="mt-2 mr-4 bg-[#826fffe3] hover:bg-[#836FFF] text-white px-[16px] shadow-md py-[8px] rounded-md font-sans "
+              onClick={openModal}
+            >
+              Add Notification
+            </button>
           </div>
-        )}
-        {showModal && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center ">
-          <div className="bg-white  rounded-md w-[400px]">
-            <div className="bg-[#8c8de0] h-[60px] rounded-md flex items-center justify-center pt-4">
-            <h2 className="text-xl font-semibold mb-4 text-[#ffffff]">
-              {editIndex !== null ? 'Edit Notification' : 'Add Notification'}
-            </h2> 
-            </div>
-                   
-                   <div className="p-3">
-                   <form onSubmit={handleSubmit}>
-              <div className="mb-4">
-                <label htmlFor="title" className="block text-sm font-medium text-gray-700">
-                  Title
-                </label>
-                <input
-                  type="text"
-                  id="title"
-                  name="title"
-                  value={notificationData.title}
-                  onChange={handleInputChange}
-                  className="mt-1 p-1 border border-gray-300 rounded-md w-full text-[13px] font-sans"
-                  required
-                />
+
+          <div className='noti-div-main'>
+            {notifications.map((notification, index) => (
+              <div
+                key={index}
+                className="noti-div"
+              >
+                <div className='noti-title'>
+                  <h2 className="text-lg ">{notification.title}</h2>
+                </div>
+                <div className="py-3 px-4 flex items-left  h-[80px]">
+                  <p>{notification.body}</p>
+                </div>
+                <div className="flex justify-end mt-2 mx-4 my-3">
+                  <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white px-[10px] py-[5px] rounded-md mr-2 text-[12px] font-sans"
+                    onClick={() => handleEdit(index)}
+                  >
+                    Edit
+                  </button>
+                  <button
+                    className="bg-[#ec7d7d] hover:bg-[#ee6161] text-white px-[10px] py-[5px] rounded-md text-[12px] font-sans"
+                    onClick={() => handleDelete(index)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
-              <div className="mb-4">
-                <label htmlFor="body" className="block text-sm font-medium text-gray-700">
-                  Body
-                </label>
-                <textarea
-                  id="body"
-                  name="body"
-                  value={notificationData.body}
-                  onChange={handleInputChange}
-                  className="mt-1 p-1 border border-gray-300 rounded-md w-full text-[13px] font-sans"
-                  required
-                ></textarea>
-              </div>
-              <div className="flex justify-end">
-                <button
-                  type="button"
-                  className="bg-[#a7a5a5] hover:bg-[#767474] text-white px-4 py-2 rounded-md mr-2"
-                  onClick={closeModal}
-                >
-                  Close
-                </button>
-                <button
-                  type="submit"
-                  className="bg-[#8c8de0] hover:bg-[#6b6dce] text-white px-4 py-2 rounded-md"
-                >
-                  {editIndex !== null ? 'Save Changes' : 'Add'}
-                </button>
-              </div>
-            </form>
-                   </div>
-           
+            ))}
           </div>
         </div>
       )}
-      
-  </div>
-  )
-}
 
-export default AdminSettings
+      <Modal show={showModal} onHide={closeModal} dialogClassName="custom-modal">
+      <Modal.Header className="custom-modal-header">
+          <Modal.Title>{editIndex !== null ? 'Edit Notification' : 'Add Notification'}</Modal.Title>
+        </Modal.Header>
+        
+        <Modal.Body className='noti-body'>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                Title
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                value={notificationData.title}
+                onChange={handleInputChange}
+                className="noti-input"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="body" className="block text-sm font-medium text-gray-700">
+                Message
+              </label>
+              <textarea
+                id="body"
+                name="body"
+                value={notificationData.body}
+                onChange={handleInputChange}
+                className="noti-input2"
+                required
+              ></textarea>
+            </div>
+
+            <div className='Noti-btn-div'>
+            <button type="submit" className='Noti-btn'>
+            {editIndex !== null ? 'Save Changes' : 'Add'}
+            </button>
+            <button type="submit" className='Noti-btn2'  onClick={closeModal}>
+            Cancel
+            </button>
+          </div>
+            {/* <div className="flex justify-end">
+            <button type="submit" className="bg-[#8c8de0] hover:bg-[#6b6dce] text-white px-4 py-2 rounded-md">
+                {editIndex !== null ? 'Save Changes' : 'Add'}
+              </button>
+              <button
+                type="button"
+                className="bg-[#a7a5a5] hover:bg-[#767474] text-white px-4 py-2 rounded-md mr-2"
+                onClick={closeModal}
+              >
+                Close
+              </button>
+              
+            </div> */}
+          </form>
+        </Modal.Body>
+      </Modal>
+    </div>
+  );
+};
+
+export default AdminSettings;
